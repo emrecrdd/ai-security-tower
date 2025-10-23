877dc94e38f63a4dc329c31a46914a7496bb30ca
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-10-23 16:46:08 +0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Veritabanı adı
-  process.env.DB_USER,     // Kullanıcı adı
-  process.env.DB_PASSWORD, // Şifre
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    logging: false, // true olursa SQL loglarını görebilirsin
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // NeonDB için gerekli
+    },
+  },
+});
 
 module.exports = sequelize;
