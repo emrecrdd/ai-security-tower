94277ed28f61c13d58dfc314f18b9edbdb458d37
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-10-24 10:45:48 +0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_BACKEND_URL;
-
+const API_BASE = import.meta.env.VITE_BACKEND_URL;
 export const api = axios.create({
   baseURL: API_BASE,
   headers: {
