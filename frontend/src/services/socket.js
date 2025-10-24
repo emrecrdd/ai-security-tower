2906a3e0d66b37f1db7ec09f5fa2334eec02bf16
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-10-24 10:45:19 +0300

    Change socket URL to use VITE_SOCKET_URL

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL; // <-- Burada process.env kullan
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+
 
 export const socket = io(SOCKET_URL);
 
